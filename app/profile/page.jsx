e8344d46ba12ac992a97d8adc4c3772c16ea980d
fatimamammadova <fatimammmadova@gmail.com>
2024-03-13 12:28:04 +0400

    profile-page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,10 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`api/users/${session?.user.id}/posts`);
+      const data = await response.json();
 
-const Profile = () => {
+      setPosts(data);
+    };
+    if(session?.user.id) fetchPosts();
+  }, []);
 
-    return (
-        <div>profile</div>
-    )
-}
+  const handleEdit = () => {};
 
-export default Profile
+  const handleDelete = () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
