c28702be0e5dc41be272f2ada1bd985640db90e2
fatimamammadova <fatimammmadova@gmail.com>
2024-03-15 10:31:52 +0400

    ...

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -46,7 +46,7 @@ export const PromptCard = ({
 
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post.creator.fullName}
             </h3>
             <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
